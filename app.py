02e74e512ba9ae0f60efb6e7f86714eef63157b5
bmcculley <box76135@gmail.com>
2018-04-02 22:49:14 -0400

    refactoring start
    so far merging some files together, updating argparse, updating to run on python 3

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import json
 import argparse
 import requests
 
+from angular_flask import app
 from angular_flask.core import db
 from angular_flask.models import Post
 
@@ -22,36 +23,43 @@ def create_db():
 def drop_db():
     db.drop_all()
 
+def runserver():
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
+
 
 def main():
     parser = argparse.ArgumentParser(
-        description='Manage this Flask application.')
-    parser.add_argument(
-        'command', help='the name of the command you want to run')
-    parser.add_argument(
-        '--seedfile', help='the file with data for seeding the database')
+        description='Manage the Flask application.')
+    parser.add_argument('-c', '--create', action='store_true',
+            help='Create the application database.')
+    parser.add_argument('-d', '--delete', action='store_true',
+            help='Delete the application database.')
+    parser.add_argument('-s', '--seedfile', dest="seedfile",
+            help='The file with data for seeding the database.')
     args = parser.parse_args()
 
+    # runserver()
     if args.command == 'create_db':
         create_db()
+        print("DB created!")
 
-        print "DB created!"
     elif args.command == 'delete_db':
         drop_db()
+        print("DB deleted!")
 
-        print "DB deleted!"
     elif args.command == 'seed_db' and args.seedfile:
         with open(args.seedfile, 'r') as f:
             seed_data = json.loads(f.read())
 
         for item_class in seed_data:
             items = seed_data[item_class]
-            print items
+            print(items)
             for item in items:
                 print item
                 create_sample_db_entry('api/' + item_class, item)
 
-        print "\nSample data added to database!"
+        print("\nSample data added to database!")
     else:
         raise Exception('Invalid command')
 
